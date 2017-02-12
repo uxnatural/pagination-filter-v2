f50f93931d1147dfbd812790d72ed7c3ef52ca3d
uxnatural <todd@uxnatural.com>
2017-02-12 15:38:11 -0700

    Weekend updates

--- js/script.js
+++ js/script.js
@@ -1,14 +1,79 @@
-var studentCount = document.getElementById('student-list').children.length;
-var showItems = 10;
-var pageItems = showItems - 1;
-var pages = [];
+
 
 //When the page loads, your program should hide all but the first 10 students in the list.
-window.onload = function () {
-    for(i = (studentCount - 1); i > pageItems; i--) {
+//window.onload = function () {
+    
+    //create an initial array of students
+    var studentArray = [];    
+    var studentCount = document.getElementById('student-list').children.length;
+    var showItems = 10;
+    var pageItems = showItems - 1;
+    var pages = new Array ();
+    var pageNumber = 0;
+
+    //load students references into studentArray
+    var loadStudents = function loadStudents(){
+        for(i = 0; i < studentCount; i++){
+            studentArray.push(i);
+        }
+        console.log("students are loaded");
+    }
+    loadStudents();
+ 
+    //function that will group the studentArray into pages based on the # of students and the desired items shown
+    var createGroupedArray = function(arr, chunkSize) {
+        pages = [];
+        var groups = [], i;
+        for (i = 0; i < arr.length; i += chunkSize) {
+            groups.push(arr.slice(i, i + chunkSize));
+        }
+        pages.push(groups);
+        console.log('pages are set');
+    }
+    createGroupedArray(studentArray,showItems);
+    
+
+    //insert searchbox input
+    document.getElementById('student-search').innerHTML = '<input type="text" id="search" class="search" placeholder="Search for students..." /><button>Search</button>';
+    
+    //insert pagination buttons
+    for(i = pages[0].length; i > 0; i--){
+        var lis = document.getElementById('pagination');
+        var pagination = '<li><a class="active" href="#">' + i + '</a></li>';
+        lis.insertAdjacentHTML('afterbegin',pagination);
+    }
+
+    document.getElementsByTagName("A").addEventListener("click", function(){
+        console.log(this.innerHTML);
+    });
+    
+    //showPageItems function
+    function showPageItems(){
+         //hide all items
+        var pageSelected = pages[0][pageNumber];
+        //hide all items
+        for(i = 0; i < studentCount; i++){
+            var hidden = document.getElementById('student-list').children[i].style.display = 'none';
+        }
+        //show only items for the selected page
+        if (pageSelected !== undefined){
+            pageSelected.forEach(function(element) {
+                var show = document.getElementById('student-list').children[element].style.display = 'block';
+            });
+        } else {
+            console.log("out of bounds")
+        }
+
+    }
+
+    //hide all but the first page of students
+/*
+    for(i = 53; i > pages[0][0].length; i--) {
         var hidden = document.getElementById('student-list').children[i].style.display = 'none';
     }
-}
+    console.log("windows is loaded");
+//}
+/* hide stuff
 function pageTurn(pageNumber){
     var rangeEnd = Math.floor((pages - pageNumber)*10);
     var rangeStart = pageNumber * pageItems;
@@ -24,26 +89,8 @@ function pageTurn(pageNumber){
        console.log(i);
     }
 }
-//create an initial array of students
-var studentArray = [];
-
-//load students references into studentArray
-var getStarted = function(){
-    for(i = 0; i < studentCount; i++){
-        studentArray.push(i);
-    }
-}
-
-//function that will group the studentArray into pages based on the # of students and the desired items shown
-var createGroupedArray = function(arr, chunkSize) {
-    pages = [];
-    var groups = [], i;
-    for (i = 0; i < arr.length; i += chunkSize) {
-        groups.push(arr.slice(i, i + chunkSize));
-    }
-    pages.push(groups);
-}
-
+*/
+/*
 var input = document.getElementById('search');
 input.onkeyup = function () {
     var filter = input.value.toUpperCase();
@@ -56,11 +103,5 @@ input.onkeyup = function () {
             lis[i].style.display = 'none';
     }
 }
-
-//create an array of the li of the ul
-//filter ul from items 0-9 or the first 10 items
-//add event listener to paging buttons
-//dynamically add paging buttons for each set of 10 or less
-
-//add event handler to "search" ul by input value against name and maybe email and show all list items that match
+*/
 
